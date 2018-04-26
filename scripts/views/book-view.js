7de8849c02e260eb95cd77c290c8dac831741f54
Stephen <stephendeanharper@gmail.com>
2018-04-26 13:18:35 -0700

    tried correcting book-view.js

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -9,10 +9,10 @@ Handlebars.compile($('book-template'))
 (function(module) {
 
 // Define a method on bookView called initIndexPage which hides any element with a class of container, shows any element with a class of book-view, and maps over the Book instances stored in Book.all to render each and append them to an element with the id of book-list.
-app.bookView.initIndexPage (
-  $('.container').hide();
-  $('.container').append(Book.all).getElementbyId('#book-list');
-);
+app.bookView.initIndexPage = function() {(
+  $('.container').hide(),
+  $('.container').append(Book.all).getElementbyId('#book-list'),
+)}
 
 // Using jQuery's Document.ready functionality, invoke Book.fetchAll when the DOM has loaded, and pass bookView.initIndexPage as it's argument.
 Document.ready(e) {
